35b25a25af0c1db05a715033298400b752453b26
GuangJian Chen <evil000@users.noreply.github.com>
2023-05-06 21:39:01 +0800

    111
    111

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -49,15 +49,15 @@ onMounted(() => {
           此项目开源于
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://github.com/evil000/aichat"
             target="_blank"
           >
             Github
           </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          ，基于 MIT 协议。
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
+          项目使用过程如有建议,请发邮件联系：miles@binsy.cn
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
